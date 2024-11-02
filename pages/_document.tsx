4dc985aaca87ac9542b67a7ee0901d734a585df0
Pedro Ribeiro <pribeiro@extensiv.com>
2024-11-02 07:32:06 -0300

    chore: add eslint as a dev dependency and update document title in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
     render() {
         return (
-            <Html lang="en">
-                <Head>
+            <Html lang="en" title='Vai ver o que?'>
+                <Head title='Vai ver o que?'>
                     {/* Include external stylesheets or meta tags */}
                     <link
                         rel="stylesheet"
